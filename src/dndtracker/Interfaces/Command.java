4a82619d6dde872a616a54eb073be400bf51bc7e
Michael Gairns <mcgairns@gmail.com>
2022-08-25 17:45:48 -0700

    add serializable interface

--- src/dndtracker/Interfaces/Command.java
+++ src/dndtracker/Interfaces/Command.java
@@ -1,5 +1,7 @@
 package dndtracker.Interfaces;
 
+import dndtracker.DataTypes.Receiver;
+
 public interface Command {
 	public void execute(Receiver receiver); // executes the command
 	public boolean command(String toCompare); // returns true if the provided string matches the command call pattern
